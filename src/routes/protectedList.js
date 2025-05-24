90ac7307666c3c4a6d0d0a4bf41e55b6b4f623f4
우은식 <apkool12@naver.com>
2025-05-24 16:51:51 +0900

    [Feat} - protect routes with UserValidate

--- src/routes/protectedList.js
+++ src/routes/protectedList.js
@@ -4,16 +4,17 @@ import ArticleDetail from "../page/ArticleDetail";
 import LevelCheck from "../page/LevelCheck";
 import LevelTest from "../page/LevelTest";
 import ContentsMain from "../page/ContentsMain";
+import UserValidate from "../routes/UserValidate"; // 경로 확인 필요
 
 const protectedRoutes = [
-  { path: "/level-check", element: <LevelCheck /> },
-  { path: "/level-test", element: <LevelTest /> },
-  // { path: "/articles/:uuid", element: <ArticleDetail /> }, // api 연결시 활용
-  { path: "/article", element: <ArticleDetail /> },
-  { path: "/dummy", element: <div>Dummy</div> },
-  { path: "/main", element: <ContentsMain /> },
-  { path: "/trend", element: <div>트렌트페이지</div> },
-  { path: "/bookmark", element: <div>북마크페이지</div> },
+  { path: "/level-check", element: <UserValidate><LevelCheck /></UserValidate> },
+  { path: "/level-test", element: <UserValidate><LevelTest /></UserValidate> },
+  // { path: "/articles/:uuid", element: <UserValidate><ArticleDetail /></UserValidate> }, // api 연결시 활용
+  { path: "/article", element: <UserValidate><ArticleDetail /></UserValidate> },
+  { path: "/dummy", element: <UserValidate><div>Dummy</div></UserValidate> },
+  { path: "/main", element: <UserValidate><ContentsMain /></UserValidate> },
+  { path: "/trend", element: <UserValidate><div>트렌트페이지</div></UserValidate> },
+  { path: "/bookmark", element: <UserValidate><div>북마크페이지</div></UserValidate> },
 ];
 
 export default protectedRoutes;
